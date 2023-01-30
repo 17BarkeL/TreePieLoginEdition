ae3bb7c8560f46ca51b869f301531fad67173ea4
Luke Barkess <luke.barkess@icloud.com>
2023-01-30 11:17:33 +0000

    throw me

---
+++ Server/static/app.js
@@ -0,0 +1,45 @@
+﻿let elements = {
+    message: document.querySelector("#message"),
+    username: document.querySelector("#username"),
+    password: document.querySelector("#password")
+}
+
+function reset() {
+
+}
+
+function createUser() {
+
+}
+
+function login() {
+    checkDetails();
+}
+
+function checkDetails() {
+    let username = elements.username.value;
+    let password = elements.password.value;
+
+    try {
+        presenceCheck(username, "Username cannot be empty");
+        presenceCheck(password, "Password cannot be empty");
+        lengthCheck(password, 4, 12, "Password must be between 4 and 12 characters");
+        elements.message.innerText = "Authenticating";
+    }
+
+    catch (e) {
+        elements.message.innerText = e;
+    }
+}
+
+function presenceCheck(input, error) {
+    if (input.length == 0) {
+        throw error;
+    }
+}
+
+function lengthCheck(input, min, max, error) {
+    if (input.length > max || input.length < min) {
+        throw error;
+    }
+}
