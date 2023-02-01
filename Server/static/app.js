159e0a05cd78de80174871a5d524aa655bf0a3ac
17BarkeL <58078420+17BarkeL@users.noreply.github.com>
2023-02-01 18:24:34 +0000

    tesasdfasf

--- Server/static/app.js
+++ Server/static/app.js
@@ -2,8 +2,19 @@
     message: document.querySelector("#message"),
     username: document.querySelector("#username"),
     password: document.querySelector("#password"),
-    passwordConfirm: document.querySelector("#password-confirm")
+    passwordConfirm: document.querySelector("#password-confirm"),
+    logo: document.querySelector("#logo")
 }
+
+timesLogoClicked = 0;
+
+logo.addEventListener(() => {
+    timesLogoClicked++;
+
+    if (timesLogoClicked == 50) {
+        logo.setAttribute("src", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi5.walmartimages.com%2Fasr%2Ff9d58c4e-436d-44fa-a0ae-4c06af347d9a_1.78bce77e95603ad61c7f098b1025f79f.jpeg&f=1&nofb=1&ipt=01c3a1d2aa37ca215be50fd16e1ae02e4a1aedcf9f00a634c9d54abf5fc648c8&ipo=images");
+    }
+});
 
 function reset() {
 
